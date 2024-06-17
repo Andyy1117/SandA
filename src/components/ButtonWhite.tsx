2cd54867d6fd2318c69524df52f7d9c1b7ae1bc5
Agar Avarzed <aavarzed24@wooster.edu>
2024-06-17 19:36:38 +0800

    fixed button

--- src/components/ButtonWhite.tsx
+++ src/components/ButtonWhite.tsx
@@ -10,7 +10,7 @@ export default function ButtonLink({
   return (
     <PrismicNextLink
       className={clsx(
-        "focus:ring-offset-3 relative inline-flex h-fit w-fit rounded-full border border-slate-600 bg-white px-4 py-2 outline-none ring-slate-300 text-black transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-blue-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-blue-200/40 hover:text-blue-600 after:hover:bg-opacity-15 focus:ring-2",
+        "focus:ring-offset-3 relative inline-flex h-fit w-fit rounded-full bg-white px-4 py-2 outline-none ring-slate-300 text-black transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-blue-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-blue-200/40 hover:text-blue-600 after:hover:bg-opacity-15 focus:ring-2",
         className,
       )}
       {...restProps}
